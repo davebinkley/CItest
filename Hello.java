e98f5d6f89a4efb53ab58978aa844625550fcf39
David Binkley <>
2024-08-08 13:25:05 -0400

    fixed typeos

--- Hello.java
+++ Hello.java
@@ -1,7 +1,9 @@
 class Hello
+{
+  public static void main(String [] args)
   {
-    public static voic main(String [] args)
-    {
-      System.out.println("hello world!")
-    }
+    System.out.println("hello Sponge Bob!");
   }
+}
+
+// https://www.youtube.com/watch?v=eCOv4AeC6Ic
